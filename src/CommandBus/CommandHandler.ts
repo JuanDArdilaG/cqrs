39bc8b8b6984e44753847f4097bfe910ef5abb6a
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-09-29 17:19:37 -0500

    [Remove] suscribedTo

--- src/CommandBus/CommandHandler.ts
+++ src/CommandBus/CommandHandler.ts
@@ -4,8 +4,6 @@ import { CommandUseCase } from "./CommandUseCase";
 export abstract class CommandHandler<T extends CommandUseCase<Command>> {
   constructor(readonly useCase: T) {}
 
-  abstract subscribedTo(): Command;
-
   handle(command: Command): Promise<void> {
     return this.useCase.execute(command);
   }
